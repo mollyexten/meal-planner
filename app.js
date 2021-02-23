f5ee13170fa10bd9207357558b7a5ee9936956ec
mollyexten <molly.exten@gmail.com>
2021-02-23 13:23:24 -0500

    Added a removeResults function and cleared search bar value after clicking submit in app.js. Updated structure of html to better accommodate appending divs via DOM

--- app.js
+++ app.js
@@ -2,35 +2,74 @@
 
 // Create a fetchData() function to capture search results based on ingredient
 
-
 async function fetchData(ingredient) {
-  // Eventually I will add the removeResults() function here - after I make it, obviously!
+
+  
+
   // For now the ingredient will be hardcoded in when I invoke this funciton, but eventually it will be captured via an event listener
   const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+  
   // Make the try/catch function
   try {
+    
+    // Invoke the removeResults() function to clear any existing search results
+    removeResults()
+    
+    // Access the API
     let response = await axios.get(url)
+    
+    // Store the meal data array in a variable
     let recipes = response.data.meals
+    
+    // Loop through each meal data item, storing image and dish name in variables
     recipes.forEach((recipe) => {
       let image = document.createElement("img")
       image.src = recipe.strMealThumb
       image.width = "200"
       let dish = document.createElement("p")
       dish.textContent = recipe.strMeal
+
+      // Later I will need the recipe id when I create links to the full recipes
       console.log(`recipe id for link (later) ${recipe.idMeal}`)
-      const appendDiv = document.querySelector(".container")
-      appendDiv.append(image)
-      appendDiv.append(dish)
+      
+      // Append image and dish to the DOM
+      const appendBottom = document.querySelector(".bottom")
+      appendBottom.append(image)
+      appendBottom.append(dish)
     })
+    
+    // Obligatory "return response" part
     return response
   } catch (err) {
     console.error(err)
   }
 }
 
+// Event listener for the search button
 const form = document.querySelector("form")
 form.addEventListener("submit", (e) => {
+  
+  // Prevent search bar from clearing before value can be extracted
   e.preventDefault()
-  const searchValue = document.querySelector("#search-value").value
+  
+  // Store the search value in a variable
+  let searchValue = document.querySelector("#search-value").value
+
+  // Plug the search value into the fetchData function
   fetchData(searchValue)
+
+  // Reset the form after the submit button has been clicked
+  document.querySelector("#search-value").value = ""
 })
+
+// removeResults function to take out previous search results
+function removeResults() {
+  
+  // Store the div with class "bottom" in a variable (This is where search results are displayed)
+  const bottom = document.querySelector(".bottom")
+
+  // Loop through the child elements of the div, removing each one until there are none left
+  while (bottom.lastChild) {
+    bottom.removeChild(bottom.lastChild)
+  }
+}
