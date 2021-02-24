bbe0c92c1afd9becef804dc70f5d04ddf961973a
mollyexten <molly.exten@gmail.com>
2021-02-24 10:14:33 -0500

    Added comments to recipeApp.js explaining what the code does

--- recipeApp.js
+++ recipeApp.js
@@ -14,59 +14,60 @@ async function renderRecipe(id) {
 
     // Access the API
     let response = await axios.get(recipeURL)
-    console.log(response)
     let recipe = response.data.meals[0]
+
+    // Store recipe name in variable "name"
     let name = document.createElement("h1")
     name.textContent = recipe.strMeal
-    console.log(name)
 
+    // Store recipe image in variable "image"
     let image = document.createElement("img")
     image.src = recipe.strMealThumb
     image.width = "400"
-    console.log(image)
 
+    // Store recipe instructions in variable "instructions"
     let instructions = document.createElement("p")
     instructions.textContent = recipe.strInstructions
-    console.log(instructions)
-
-    console.log("ingredients", recipe)
     
-    
+    // Append name and image to the top half of the page
     const appendTop = document.querySelector(".recipe-top")
     appendTop.append(name)
     appendTop.append(image)
     
+    // Invoke showIngredients function to access ingredients with their amounts and append to page after the image
+    showIngredients(recipe)
+    
+    // Append instructions to the bottom half of the page
     const appendBottom = document.querySelector(".recipe-bottom")
     appendBottom.append(instructions)
-    
-    showIngredients(recipe)
 
+    // Obligatory return resposne
     return response
   } catch (err) {
     console.error(err)
   }
 }
 
+// For now I am calling the function with a specific id because I don't know how to do the redirect
 renderRecipe("52997")
 
-// function showIngredients(obj, objName) {
-//   for (let key in obj) {
-//     if (key.includes("strIngredient")) {
-//       console.log(`${objName}.${key} = ${obj[key]}`)
-//     }
-//   }
-// }
-
 function showIngredients(obj) {
+  
+  // Append the ingredients table to the top half of the recipe (after the image)
   const appendTop = document.querySelector(".recipe-top")
   ingredientTable = document.createElement("table")
   appendTop.append(ingredientTable)
+
+  // Give the table the title "Ingredients"
   const header = ingredientTable.createTHead();
   let row = header.insertRow(0);
   let cell = row.insertCell(0);
   cell.textContent = "Ingredients"
+  
+  // Set up arrays for measurements and ingredients to push into later
   let measurements = []
   let ingredients = []
+  
   // I used this resource to understand for...in loops: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
   for (let key in obj) {
     // This reference showed me how to check if a string contains a substring: https://flaviocopes.com/how-to-string-contains-substring-javascript/
@@ -79,6 +80,8 @@ function showIngredients(obj) {
       ingredients.push(obj[key])
     }
   }
+
+  // Fill in the table with measurement and ingredient arrays
   for (let i = 0; i < measurements.length; i++) {
     let row = ingredientTable.insertRow(i+1)
     let cell1 = row.insertCell(0)
