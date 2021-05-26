0b085b67716caeba30f63c5d5d6c9cda60804db9
mollyexten <molly.exten@gmail.com>
2021-05-26 16:00:08 -0400

    removed domHelpers.js

--- modules/domHelpers.js
+++
@@ -1,8 +0,0 @@
-export default function createMainImage(recipeInfo) {
-  let mainImage = document.createElement("img")
-  mainImage.src = recipeInfo.strMealThumb
-  mainImage.alt = "random photo of food"
-  mainImage.id = recipeInfo.idMeal
-  mainImage.className = "background-image"
-  return mainImage
-}
