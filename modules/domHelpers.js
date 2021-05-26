3cf156b1d586bc3b4789c51d167bc996a5c55087
mollyexten <molly.exten@gmail.com>
2021-05-26 15:05:00 -0400

    testing out dom helper function

--- modules/domHelpers.js
+++ modules/domHelpers.js
@@ -1,3 +1,12 @@
 export function Hello() {
   console.log("Hello buddy!")
 }
+
+export function createMainImage(img) {
+  let mainImage = document.createElement("img")
+  mainImage.src = img.strMealThumb
+  mainImage.alt = "random photo of food"
+  mainImage.id = img.idMeal
+  mainImage.className = "background-image"
+  return mainImage
+} 
